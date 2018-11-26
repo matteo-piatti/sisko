8f96c415ca0245bb4db92c82be76bdf37c676b2b
Matteo Piattu <matteo.piatti@liip.ch>
2018-11-26 16:17:30 +0100

    Changed some stuff (best commit message ever)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
+var favicon = require('serve-favicon')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var routing = require('./routes/routing')
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', '/images/favicon.png')))
 
 
 app.use(routing)
--- routes/routing.js
+++ routes/routing.js
@@ -2,7 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Sisko - Web Solutions' })
+  res.render('index', { title: 'Sisko - Web Solutions', name: 'home' })
+})
+
+router.get('/team', function (req, res, next) {
+  res.render('team', { title: 'Sisko - Team', name: 'team' })
+})
+
+router.get('/contact', function (req, res, next) {
+  var subject = 'dini mär'
+  var text = ''
+  res.render('contact', { title: 'Sisko - Contact', name: 'contact', subject: subject, field: text, error: 'false', data: req.query })
+})
+
+router.get('/kontakt', function (req, res, next) {
+  var subject = 'dini mär'
+  var text = ''
+  res.render('contact', { title: 'Sisko - Contact', name: 'contact', subject: subject, field: text, error: 'false', data: req.query })
 })
 
 module.exports = router;
