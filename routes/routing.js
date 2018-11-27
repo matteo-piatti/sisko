dff6133be9574c2448f2cc5577cd10e0fc4ad4d8
Matteo Piattu <matteo.piatti@liip.ch>
2018-11-27 08:31:55 +0100

    Impressum

--- routes/routing.js
+++ routes/routing.js
@@ -9,16 +9,20 @@ router.get('/team', function (req, res, next) {
   res.render('team', { title: 'Sisko - Team', name: 'team' })
 })
 
+router.get('/impressum', function (req, res, next) {
+  res.render('impressum', { title: 'Sisko - Impressum', name: 'impressum' })
+})
+
 router.get('/contact', function (req, res, next) {
   var subject = 'dini mär'
   var text = ''
-  res.render('contact', { title: 'Sisko - Contact', name: 'contact', subject: subject, field: text, error: 'false', data: req.query })
+  res.render('contact', { title: 'Sisko - Kontakt', name: 'contact', subject: subject, field: text, error: 'false', data: req.query })
 })
 
 router.get('/kontakt', function (req, res, next) {
   var subject = 'dini mär'
   var text = ''
-  res.render('contact', { title: 'Sisko - Contact', name: 'contact', subject: subject, field: text, error: 'false', data: req.query })
+  res.render('contact', { title: 'Sisko - Kontakt', name: 'contact', subject: subject, field: text, error: 'false', data: req.query })
 })
 
 module.exports = router;
